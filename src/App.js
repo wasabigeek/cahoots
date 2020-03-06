11b44cab23bcc1e0e3fe671cc8f5e1802aafb528
Nicholas Koh <nkohwl@gmail.com>
2020-03-07 00:00:00 +0800

    Spike retrieving and display from Airtable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+var Airtable = require('airtable')
+Airtable.configure({endpointUrl: 'https://api.airtable.com', apiKey: process.env.REACT_APP_AIRTABLE_API_KEY})
+var base = Airtable.base('appSUNCUlDaS37SeJ')
+
+const Hello = props => {
+  const [questions, setQuestions] = useState([]);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    base('Question')
+      .select()
+      .eachPage((records, fetchNextPage) => {
+        // This function (`page`) will get called for each page of records.
+        setQuestions(records.concat(questions))
+
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage()
+        console.log(questions)
+    }, function done(err) {
+        if (err) { console.error(err); return; }
+    })
+  }, []);
+
+  console.log(questions)
+  return (
+    <div>
+      {questions.map(question => <div>{question.get('Name')}</div>)}
+    </div>
+  )
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello/>
     </div>
   );
 }
