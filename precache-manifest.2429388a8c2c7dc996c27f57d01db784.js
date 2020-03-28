959f365cf8c8174042996bb4a4d50c19cdf000d1
Nicholas Koh <nkohwl@gmail.com>
2020-03-28 18:34:20 +0800

    Updates

---
+++ precache-manifest.2429388a8c2c7dc996c27f57d01db784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e917e4191ab73efbf2b93e5d8d89b2",
+    "url": "/cahoots/index.html"
+  },
+  {
+    "revision": "4c18fbe950e6b34b3f25",
+    "url": "/cahoots/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c07ea27200d3ce11dc4d",
+    "url": "/cahoots/static/js/2.78cc9528.chunk.js"
+  },
+  {
+    "revision": "a0e9b66a2fc63cd4822787adca71d90f",
+    "url": "/cahoots/static/js/2.78cc9528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c18fbe950e6b34b3f25",
+    "url": "/cahoots/static/js/main.b89c7934.chunk.js"
+  },
+  {
+    "revision": "3e8f9c616b6339a39a3b",
+    "url": "/cahoots/static/js/runtime-main.5480bec4.js"
+  }
+]);
