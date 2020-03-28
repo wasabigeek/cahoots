9ec16cd933b2125f4c54543d1400a3ed89957b20
Nicholas Koh <nkohwl@gmail.com>
2020-03-28 23:11:59 +0800

    feat: Improve styling of HomeRoute

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import HostRoute from './routes/HostRoute'
--- src/routes/HomeRoute/index.js
+++ src/routes/HomeRoute/index.js
@@ -1,33 +1,51 @@
 import React, { useState } from 'react'
 import { Link } from "react-router-dom"
+import { Button, Col, Container, Form, FormGroup, Label, Input } from 'reactstrap';
 
 import styles from './styles.module.css'
+
+
+const generateGameId = (apiKey, baseId) => {
+  return window.btoa(JSON.stringify({apiKey, baseId}))
+}
+
+const JoinUrl = ({ apiKey, baseId }) => {
+  const gameId = generateGameId(apiKey, baseId)
+  return (
+    <FormGroup>
+      <Label>Join URL:</Label>
+      <Input
+        disabled
+        placeholder={"Enter an API Key and Base ID to generate the URL"}
+        value={apiKey && baseId ? `${window.location}games/${encodeURI(gameId)}/join` : null}
+      />
+    </FormGroup>
+  )
+}
 
 const HomeRoute = props => {
   const [apiKey, setApiKey] = useState('');
   const [baseId, setBaseId] = useState('');
-  const gameId = window.btoa(JSON.stringify({apiKey, baseId}))
+  const gameId = generateGameId(apiKey, baseId)
   return (
-    <div>
+    <Container style={{maxWidth: "500px"}}>
       <h1 className={styles.hero_heading}>Cahoots!</h1>
-      <div>
-        <label>
-          API Key:
-          <input value={apiKey} onChange={e => setApiKey(e.target.value)} />
-        </label>
-        <label>
-          Base ID:
-          <input value={baseId} onChange={e => setBaseId(e.target.value)} />
-        </label>
-      </div>
+      <Form className="mb-5">
+        <FormGroup>
+          <Label for="apiKey">API Key:</Label>
+          <Input id="apiKey" value={apiKey} onChange={e => setApiKey(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Label>Base ID:</Label>
+          <Input value={baseId} onChange={e => setBaseId(e.target.value)} />
+        </FormGroup>
+        <JoinUrl apiKey={apiKey} baseId={baseId} />
+      </Form>
+
       <Link to={`/games/${encodeURI(gameId)}/host`}>
-        <button className={styles.btn_primary}>Host Game</button>
+        <Button color="primary">Host Game</Button>
       </Link>
-      <div>
-        Join URL:&nbsp;
-        {apiKey && baseId ? `${window.location}games/${encodeURI(gameId)}/join` : 'Enter your API Key and Base ID to generate a URL'}
-      </div>
-    </div>
+    </Container>
   )
 }
 
