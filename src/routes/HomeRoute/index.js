9088b330519b0b533b080564e7db6abe0df100ed
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-07-06 23:06:46 +0800

    feat: Change the join instructions to use shortCode

--- src/routes/HomeRoute/index.js
+++ src/routes/HomeRoute/index.js
@@ -4,7 +4,12 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 import CenteredContainer from '../../view_components/CenteredContainer';
 import styles from './styles.module.css'
+import findGamesByShortCode from '../../use_cases/findGamesByShortCode';
 
+
+const findGame = () => {
+  findGamesByShortCode(2713).then(console.log)
+}
 
 const HomeRoute = props => {
   const [gameId, setGameId] = useState(null);
@@ -14,16 +19,17 @@ const HomeRoute = props => {
       <h1 className={styles.hero_heading}>Cahoots!</h1>
       <Form className="mb-5">
         <FormGroup>
-          <Label for="gameId">Game ID:</Label>
+          <Label for="gameId">Game Code:</Label>
           <Input id="gameId" value={gameId} onChange={e => setGameId(e.target.value)} />
         </FormGroup>
-        <Link to={`/join/${gameId}`}>
-          <Button color="primary" disabled={!gameId}>Find Game</Button>
-        </Link>
+        <Button color="primary" disabled={!gameId} onClick={findGame}>Find Game</Button>
       </Form>
-      <Link to="/login">
-        Sign Up
-      </Link>
+      <aside>
+        Want to create your own quiz?&nbsp;
+        <Link to="/login">
+          Sign Up
+        </Link>
+      </aside>
     </CenteredContainer>
   )
 }
--- src/routes/LobbyRoute.js
+++ src/routes/LobbyRoute.js
@@ -3,6 +3,7 @@ import { useParams, Link } from "react-router-dom"
 import { Button, ListGroup, ListGroupItem } from 'reactstrap';
 
 import getLobbyPlayers from '../use_cases/getLobbyPlayers';
+import getGame from '../use_cases/getGame';
 import CenteredContainer from '../view_components/CenteredContainer';
 
 
@@ -22,18 +23,22 @@ const PlayerList = ({ players }) => {
 const LobbyRoute = props => {
   let { gameId } = useParams();
   const [players, setPlayers] = useState([]);
-  const joinUrl = `${window.location.origin}/cahoots/#/join/${gameId}/`;
+  const [game, setGame] = useState(null);
+  const joinUrl = `${window.location.origin}/cahoots/`;
 
   useEffect(() => {
     if (gameId) {
       getLobbyPlayers(gameId, setPlayers);
+      getGame(gameId).then(setGame);
     }
   }, [gameId])
 
   return (
     <CenteredContainer verticalCentered={true} maxWidth={800}>
       <h1 className="mb-4">Waiting for Players to join...</h1>
-      <p>To join, go to <a href={joinUrl} rel="noopener noreferrer" target="_blank">{joinUrl}</a></p>
+      <p>
+        To join, go to <a href={joinUrl} rel="noopener noreferrer" target="_blank">{joinUrl}</a> and enter the game code <strong>{game ? game.shortCode : "___"}</strong>
+      </p>
       <PlayerList players={players} />
       <div className="mt-4">
         <Link to={`/host/${gameId}/questions/pending`}>
